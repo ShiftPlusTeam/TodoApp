cb226db75cadfd0de99a70126d2a5e36ad1b5d8a
cy11112 <cy11112@shibaura-it.ac.jp>
2017-06-11 11:00:46 +0900

    再コミット

--- ToDoApp/ViewController.swift
+++ ToDoApp/ViewController.swift
@@ -5,15 +5,39 @@
 //  Created by Sasakura Hirofumi on 2017/06/03.
 //  Copyright © 2017 Sasakura Hirofumi. All rights reserved.
 //  HirofumiSasakura Kasahara
-//  HirofumiSasakura55
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    //@IBOutlet var taskbord: [UItableView]!
+    var myTasks: NSMutableArray = ["1" , "2" , "3" , "4"]
+    var taskbord: UITableView!
+    
+    //@IBOutlet weak var taskbord: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        // Status Barの高さを取得する.
+        let barHeight: CGFloat = UIApplication.shared.statusBarFrame.size.height
+        
+        // Viewの高さと幅を取得する.
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        
+        // TableViewの生成(Status barの高さをずらして表示).
+        taskbord = UITableView(frame: CGRect(x: 0, y: barHeight + 50, width: displayWidth, height: displayHeight))
+
+        taskbord.register(UITableViewCell.self, forCellReuseIdentifier: "taskbord")
+        
+        taskbord.dataSource = self
+        
+        taskbord.delegate = self
+        
+        self.view.addSubview(taskbord)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +45,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Num: \(indexPath.row)")
+        print("Value: \(myTasks[indexPath.row])")
+    }
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myTasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskbord", for: indexPath as IndexPath)
+        
+        cell.textLabel!.text = "\(myTasks[indexPath.row])"
+        return cell
+    }
+    
 }
+
 
