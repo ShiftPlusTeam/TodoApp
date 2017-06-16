4930aa12d56eb989571b1174422a63ac330e462f
cy11112 <cy11112@shibaura-it.ac.jp>
2017-06-17 08:11:00 +0900

    CoreDataのインポート、AppDeregateへの追加 DBとやりとりするためのfunk作成 DBとやりとりする部分は別クラスで実装した方がいいかもしれない（MVC的な意味で） とりあえずコメント書いといたが分かりにくかったら聞いてくだち

--- ToDoApp/ViewController.swift
+++ ToDoApp/ViewController.swift
@@ -7,17 +7,18 @@
 //  HirofumiSasakura Kasahara
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    
-    var myTasks: NSMutableArray = [] //タスク群を格納する配列
+    var taskDataArray: NSMutableArray! = [] //タスク群を格納する配列
     var taskbord: UITableView!       //タスクを配置するテーブルビュー
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        self.dataLoad()
         
         // Status Barの高さを取得する.
         let barHeight: CGFloat = UIApplication.shared.statusBarFrame.size.height
@@ -32,7 +33,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         taskbord.register(UITableViewCell.self, forCellReuseIdentifier: "taskbord")
         
         taskbord.dataSource = self
-        
         taskbord.delegate = self
         
         self.view.addSubview(taskbord)
@@ -44,22 +44,80 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //coredateからデータの読み込み
+    func dataLoad() {
+        taskDataArray.removeAllObjects()
+        let appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context:NSManagedObjectContext = appDelegate.managedObjectContext
+        let fetchRequest:NSFetchRequest<TaskData> = TaskData.fetchRequest()
+        //var nameList:Array<String> = []
+        let fetchData = try! context.fetch(fetchRequest)
+        if(!fetchData.isEmpty){
+            print(fetchData)
+            for i in 0..<fetchData.count{
+                print(fetchData[i].taskname!)
+                taskDataArray.add(fetchData[i].taskname!)
+            }
+        }
+    }
+    
+    //coredateのデータ削除
+    func dataDelete() {
+    
+    }
+    
+    //coredateのデータ全削除
+    func dataAllDelete() {
+        let appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context:NSManagedObjectContext = appDelegate.managedObjectContext
+        let fetchRequest:NSFetchRequest<TaskData> = TaskData.fetchRequest()
+        let predicate = NSPredicate(format: "%K LIKE %@", "taskname" , "*")
+        fetchRequest.predicate = predicate
+        let fetchData = try! context.fetch(fetchRequest)
+        if(!fetchData.isEmpty){
+            print(fetchData)
+            for i in 0..<fetchData.count{
+                print("削除対象\(fetchData[i].taskname!)")
+                let deleteObject = fetchData[i] as TaskData
+                context.delete(deleteObject)
+            }
+            do{
+                try context.save()
+            }catch{
+                print(error)
+            }
+        }
+    }
+    
+    //coredateへのデータ追加
+    func dataAdd (_ taskText: String) {
+        let appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context:NSManagedObjectContext = appDelegate.managedObjectContext
+        let taskdata = TaskData(context: context)
+        taskdata.taskname = taskText
+        do{
+            try context.save()
+        }catch{
+            print(error)
+        }
+    }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("Num: \(indexPath.row)")
-        print("Value: \(myTasks[indexPath.row])")
+        print("Value: \(taskDataArray[indexPath.row])")
     }
 
     //セルの総数取得
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return myTasks.count
+        return taskDataArray.count
     }
     
     //各セルにタスク群を代入
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "taskbord", for: indexPath as IndexPath)
         
-        cell.textLabel!.text = "\(myTasks[indexPath.row])"
+        cell.textLabel!.text = "\(taskDataArray[indexPath.row])"
         return cell
     }
     
@@ -78,12 +136,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             //OKボタン押下でテキスト取得
                 if let textFields = alert.textFields{
                     for textField in textFields {
-                        print(textField.text!)
-                        
-                        self.myTasks.add(textField.text!)//ここでDBにレコードを追加
-                        
-                        self.taskbord.reloadData()
+                        let taskText = textField.text
+                        print("test\(taskText!)")
+                        if (taskText == ""){
+                            print("cansel")
+                        }
+                        else if (taskText == "alldelete"){
+                            self.dataAllDelete()
+                        }else{
+                            self.dataAdd(textField.text!)
+                        }
                     }
+                    self.dataLoad()
+                    self.taskbord.reloadData()
                 }
             }
         )
